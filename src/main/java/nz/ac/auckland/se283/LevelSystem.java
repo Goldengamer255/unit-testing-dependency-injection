bd7fb62790935c21082cba2e00903e64f3208b34
Tanush <tpan459@aucklanduni.ac.nz>
2025-10-12 16:37:43 +1300

    test(story1): add failing test for getConcept using level 1 and difficulty 1

---
+++ src/main/java/nz/ac/auckland/se283/LevelSystem.java
@@ -0,0 +1,9 @@
+package nz.ac.auckland.se283;
+
+public class LevelSystem {
+
+  public String getConceptForLevel(int i, int j) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getConceptForLevel'");
+  }
+}
---
+++ src/test/java/nz/ac/auckland/se283/LevelSystemTest.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.se283;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class LevelSystemTest {
+
+  @Test
+  void getConceptForlevel_level1Difficulty3_returnsConditionalStatements() {
+    LevelSystem ls = new LevelSystem();
+    String concept = ls.getConceptForLevel(1, 1);
+    assertEquals("Variables and Data Types", concept);
+  }
+}
