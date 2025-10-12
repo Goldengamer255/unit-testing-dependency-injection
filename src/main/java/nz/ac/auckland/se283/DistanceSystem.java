8d4e8f1fab31933bd006da0bb607af6e46abeb5c
Tanush <tpan459@aucklanduni.ac.nz>
2025-10-12 22:57:34 +1300

    feat(Story1): implement default values to pass test

--- src/main/java/nz/ac/auckland/se283/DistanceSystem.java
+++ src/main/java/nz/ac/auckland/se283/DistanceSystem.java
@@ -1,14 +1,24 @@
 package nz.ac.auckland.se283;
 
 public class DistanceSystem {
+  private int distance;
+  private int degrees;
 
-  public Object getDistance() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getDistance'");
+  public DistanceSystem() {
+    this.distance = 5;
+    this.degrees = 0;
+  }
+
+  public DistanceSystem(int distance, int degrees) {
+    this.distance = distance;
+    this.degrees = degrees;
+  }
+
+  public int getDistance() {
+    return distance;
   }
 
   public Integer getDegrees() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getDegrees'");
+    return degrees;
   }
 }
