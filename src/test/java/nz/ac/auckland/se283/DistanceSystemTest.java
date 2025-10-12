31bd2fd5025c734406be68c14af3c1c6501d9f2e
Tanush <tpan459@aucklanduni.ac.nz>
2025-10-12 23:00:32 +1300

    refactor(Story1): clean up Distance tests  after passing test

--- src/test/java/nz/ac/auckland/se283/DistanceSystemTest.java
+++ src/test/java/nz/ac/auckland/se283/DistanceSystemTest.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se283;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -9,13 +10,8 @@ public class DistanceSystemTest {
   void getDistance_whenDefault_distanceIsZero() {
     DistanceSystem distance = new DistanceSystem();
 
-    assertEquals(5, distance.getDistance());
-  }
-
-  @Test
-  void getDegrees_whenDefault_degreesIsZero() {
-    DistanceSystem distance = new DistanceSystem();
-
-    assertEquals(0, distance.getDegrees());
+    assertAll(
+        () -> assertEquals(5, distance.getDistance()),
+        () -> assertEquals(0, distance.getDegrees()));
   }
 }
