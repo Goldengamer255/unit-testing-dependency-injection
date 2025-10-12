7c2f789fe8ee07551997259579126bdac231d138
Tanush <tpan459@aucklanduni.ac.nz>
2025-10-12 19:11:11 +1300

    feat(Story1): implement test 3 concept to pass test

--- src/main/java/nz/ac/auckland/se283/LevelSystem.java
+++ src/main/java/nz/ac/auckland/se283/LevelSystem.java
@@ -4,12 +4,13 @@ public class LevelSystem {
 
   private static final String level1_concept = "Variables and Data Types";
   private static final String level2_concept = "Operators and Expressions";
+  private static final String level3_concept = "Conditional Statements";
 
   public String getConceptForLevel(int level) {
-    if (level == 1) {
-      return level1_concept;
-    } else if (level == 2) {
+    if (level == 2) {
       return level2_concept;
+    } else if (level == 3) {
+      return level3_concept;
     }
     return level1_concept;
   }
