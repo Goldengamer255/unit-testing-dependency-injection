e853d9984bfe8366586a1eb3749f221aa499e51a
Tanush <tpan459@aucklanduni.ac.nz>
2025-10-12 17:08:01 +1300

    feat(story1): Implement Get concept behaviour for level 1 to pass test

--- src/main/java/nz/ac/auckland/se283/LevelSystem.java
+++ src/main/java/nz/ac/auckland/se283/LevelSystem.java
@@ -3,7 +3,6 @@ package nz.ac.auckland.se283;
 public class LevelSystem {
 
   public String getConceptForLevel(int i) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getConceptForLevel'");
+    return "Variables and Data Types";
   }
 }
