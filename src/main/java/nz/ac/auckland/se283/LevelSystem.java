eb957945717fee66661dda23bd5a2b6d05c3029d
Tanush <tpan459@aucklanduni.ac.nz>
2025-10-12 18:55:26 +1300

    refactor(story1): clean up LevelSystem and LevelSystemTest after passing test

--- src/main/java/nz/ac/auckland/se283/LevelSystem.java
+++ src/main/java/nz/ac/auckland/se283/LevelSystem.java
@@ -2,7 +2,9 @@ package nz.ac.auckland.se283;
 
 public class LevelSystem {
 
-  public String getConceptForLevel(int i) {
-    return "Variables and Data Types";
+  private static final String level1_concept = "Variables and Data Types";
+
+  public String getConceptForLevel(int level) {
+    return level1_concept;
   }
 }
--- src/test/java/nz/ac/auckland/se283/LevelSystemTest.java
+++ src/test/java/nz/ac/auckland/se283/LevelSystemTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test;
 public class LevelSystemTest {
 
   @Test
-  void getConceptForlevel_level1Difficulty3_returnsConditionalStatements() {
-    LevelSystem ls = new LevelSystem();
-    String concept = ls.getConceptForLevel(1);
+  void getConceptForlevel_level1_returnsConditionalStatements() {
+    LevelSystem level = new LevelSystem();
+    String concept = level.getConceptForLevel(1);
     assertEquals("Variables and Data Types", concept);
   }
 }
