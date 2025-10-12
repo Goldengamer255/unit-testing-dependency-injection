7484c4afa08d2f1a4040a718ae8bb093a3e29ef0
Tanush <tpan459@aucklanduni.ac.nz>
2025-10-12 19:06:05 +1300

    feat(story1): implemented the 2nd concept in the code to pass test

--- src/main/java/nz/ac/auckland/se283/LevelSystem.java
+++ src/main/java/nz/ac/auckland/se283/LevelSystem.java
@@ -3,8 +3,14 @@ package nz.ac.auckland.se283;
 public class LevelSystem {
 
   private static final String level1_concept = "Variables and Data Types";
+  private static final String level2_concept = "Operators and Expressions";
 
   public String getConceptForLevel(int level) {
+    if (level == 1) {
+      return level1_concept;
+    } else if (level == 2) {
+      return level2_concept;
+    }
     return level1_concept;
   }
 }
